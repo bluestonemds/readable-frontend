5a79902dd185f83cb8394b9d8b05139dba2be802
王晓峰 <wangxiaofeng@wangxiaofengs-iMac.local>
2018-03-03 20:38:25 +0800

    first commit.

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "../assets/css/bootstrap.css"
+import "../assets/css/bootstrap-grid.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+       <h1 className="text-success">readable project</h1>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
